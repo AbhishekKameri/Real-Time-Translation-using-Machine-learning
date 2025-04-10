d78704a3255c353a1daf79a346e730d2c3acd3f5
AbhishekKameri <122601478+AbhishekKameri@users.noreply.github.com>
2025-04-10 16:52:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,16 @@ def translate_text(text, target_lang):
 
     return "Unsupported language selected."
 
+def text_to_speech(text, lang_code):
+    try:
+        tts = gTTS(text=text, lang=lang_code)
+        tts.save("static/translated_audio.mp3")
+        return True
+    except Exception as e:
+        print("TTS Error:", e)
+        return False
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -55,15 +65,16 @@ def index():
     selected_lang = "hi"
 
     if request.method == 'POST':
-        selected_lang = request.form.get("language", "hi")
         input_text = request.form.get("input_text", "").strip()
+        selected_lang = request.form.get("language", "hi")
 
         if input_text:
             translation = translate_text(input_text, selected_lang)
-        else:
-            translation = "Please enter some text to translate."
 
     return render_template('index.html', translation=translation, selected_lang=selected_lang)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
